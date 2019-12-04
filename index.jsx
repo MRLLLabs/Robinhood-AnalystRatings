79b6c8fe5b8b1835eab8ea52e02f094e8b844b1d
Kelly Willard <32944774+kwillard95@users.noreply.github.com>
2019-12-03 23:19:14 -0800

    Progress bars render dynamically

---
+++ index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Ratings from './Ratings.jsx';
+
+ReactDOM.render(
+    <Ratings/>,
+    document.getElementById('root')
+  );
+  
