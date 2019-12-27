dc9482484779a5cfd70b5009274424c87a34cc10
Desmond Rogers <desmondrogers2@gmail.com>
2019-12-26 16:28:47 -0800

    Create a dates generator to optimize analyst ratings generator

--- database/generators/analyst-rating.js
+++ database/generators/analyst-rating.js
@@ -1,7 +1,7 @@
-const faker = require('faker');
 const fs = require('fs');
 const path = require('path');
 const moment = require('moment');
+const dates = require('./dates');
 
 const getRandomIdxInclusive = (n, x) => {
   const min = Math.ceil(n);
@@ -12,23 +12,34 @@ const getRandomIdxInclusive = (n, x) => {
 const ratings = [null, 'buy', 'hold', 'sell'];
 const analystsRatingsFields = 'id,analyst_id,stock_id,rating,rating_date\n';
 
+fs.writeFileSync(
+  path.resolve(__dirname, '..', 'analysts-ratings.csv'),
+  analystsRatingsFields,
+  err => console.log(err)
+);
+
+const datesCount = dates.length;
+
 let i = 1e8;
 while (i) {
   const id = i;
   const randomAnalystId = getRandomIdxInclusive(1, 1e6);
   const randomStockId = getRandomIdxInclusive(17920338, 27920337);
   const randomRating = ratings[getRandomIdxInclusive(0, 3)];
-  const randomRatingDate = moment(faker.date.recent(365)).format('YYYY-MM-DD');
+  const randomRatingDate = moment(
+    dates[getRandomIdxInclusive(0, datesCount)]
+  ).format('YYYY-MM-DD');
 
+  /* CSV */
   const analystRating = `${id},${randomAnalystId},${randomStockId},${randomRating},${randomRatingDate}\n`;
 
   fs.appendFileSync(
     path.resolve(__dirname, '..', 'analysts-ratings.csv'),
-    i === 1 ? `${analystsRatingsFields}${analystRating}` : analystRating,
+    analystRating,
     err => console.log(err)
   );
 
-  if (i % 10000 === 0) {
+  if (i % 10000 === 0 || i < 10000) {
     console.log(i);
   }
 
