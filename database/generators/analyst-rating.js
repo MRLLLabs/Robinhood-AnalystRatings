e1714077c4132ba2353d4ac938ff931b1ff0d388
Desmond Rogers <desmondrogers2@gmail.com>
2019-12-28 20:23:39 -0800

    Refactor generator
    Generator will now output a tab delimited txt file and split the data
    into smaller more manageable sizes

--- database/generators/analyst-rating.js
+++ database/generators/analyst-rating.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const moment = require('moment');
-const dates = require('./dates');
+const faker = require('faker');
 
 const getRandomIdxInclusive = (n, x) => {
   const min = Math.ceil(n);
@@ -10,30 +10,29 @@ const getRandomIdxInclusive = (n, x) => {
 };
 
 const ratings = ['buy', 'hold', 'sell'];
-const datesCount = dates.length;
 
+let chunk = 1;
 let i = 1e8;
 while (i) {
   const id = i;
   const randomAnalystId = getRandomIdxInclusive(1, 1e6);
   const randomStockId = getRandomIdxInclusive(17920338, 27920337);
   const randomRating = ratings[getRandomIdxInclusive(0, 2)];
-  const randomRatingDate = moment(
-    dates[getRandomIdxInclusive(0, datesCount)]
-  ).format('YYYY-MM-DD');
+  const randomRatingDate = moment(faker.date.recent(365)).format('YYYY-MM-DD');
 
-  /* CSV */
-  const analystRating = `${id},${randomAnalystId},${randomStockId},${randomRating},${randomRatingDate}\n`;
+  /* Tab delimited row */
+  const analystRating = `${id}\t${randomAnalystId}\t${randomStockId}\t${randomRating}\t${randomRatingDate}\n`;
 
   fs.appendFileSync(
-    path.resolve(__dirname, '..', 'analysts-ratings.csv'),
+    path.resolve(__dirname, '..', `sample_data/analysts-ratings-${chunk}.txt`),
     analystRating,
     err => console.log(err)
   );
 
-  if (i % 10000 === 0 || i < 10000) {
-    console.log(i);
-  }
-
   i -= 1;
+
+  if (i % 1e7 === 0) {
+    console.log(`Chunk ${chunk} complete`);
+    chunk += 1;
+  }
 }
