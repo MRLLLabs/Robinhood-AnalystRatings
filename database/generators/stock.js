c542e8391a5d6599de5e53f1388d4b13ca1bf5fe
Desmond Rogers <desmondrogers2@gmail.com>
2020-01-07 14:28:57 -0800

    Refactor generator in prep for protobuf streaming

--- database/generators/stock.js
+++ database/generators/stock.js
@@ -36,33 +36,57 @@ const tickers = Combinatorics.baseN(
   .toArray()
   .map(cmb => cmb.join(''));
 
-for (let i = 0; i < 1e7; i += 1) {
-  const randomBuyRatingPercentage = Math.random();
-  const randomSellRatingPercentage =
-    Math.random() * (1 - randomBuyRatingPercentage);
-  const randomHoldRatingPercentage =
-    1 - (randomBuyRatingPercentage + randomSellRatingPercentage);
+const writeStocks = (writer, enc, cb) => {
+  console.log('started...');
+  let i = 1e7;
 
-  const roundOffTwoDecimals = number => Math.round(number * 100) / 100;
+  const write = () => {
 
-  const stock = {
-    symbol: tickers[i],
-    company: faker.company.companyName(),
-    description: faker.lorem.paragraph(),
-    sellSummary: faker.lorem.paragraph(2),
-    buySummary: faker.lorem.paragraph(2),
-    buyRating: roundOffTwoDecimals(randomBuyRatingPercentage),
-    sellRating: roundOffTwoDecimals(randomSellRatingPercentage),
-    holdRating: roundOffTwoDecimals(randomHoldRatingPercentage),
-  };
+    let ok = true;
+    do {
+      i -= 1;
+      const randomBuyRatingPercentage = Math.random();
+      const randomSellRatingPercentage =
+        Math.random() * (1 - randomBuyRatingPercentage);
+      const randomHoldRatingPercentage =
+        1 - (randomBuyRatingPercentage + randomSellRatingPercentage);
 
-  fs.appendFileSync(
-    path.resolve(__dirname, '..', 'stocks.json'),
-    `${JSON.stringify(stock)}\n`,
-    err => console.log(err)
-  );
+      const roundOffTwoDecimals = number => Math.round(number * 100) / 100;
 
-  if (i % 5000 === 0) {
-    console.log(i);
+      const stock = {
+        symbol: tickers[i],
+        company: faker.company.companyName(),
+        description: faker.lorem.paragraph(),
+        sellSummary: faker.lorem.sentences(2, '. '),
+        buySummary: faker.lorem.sentences(2, '. '),
+        buyRating: roundOffTwoDecimals(randomBuyRatingPercentage),
+        sellRating: roundOffTwoDecimals(randomSellRatingPercentage),
+        holdRating: roundOffTwoDecimals(randomHoldRatingPercentage),
+      };
+
+      // see if we should continue, or wait
+      // don't pass the callback, because we're not done yet.
+      if (i === 0) {
+        writer.write(`${JSON.stringify(stock)}\n`, enc, cb);
+      } else {
+        ok = writer.write(`${JSON.stringify(stock)}\n`, enc);
+      }
+
+    } while (i && ok);
+
+    if (i > 0) {
+      // had to stop early!
+      // write some more once it drains
+      writer.once('drain', write);
+    }
   }
+
+  write();
 }
+
+const stream = fs.createWriteStream(path.resolve(__dirname, '../sample_data/stocks.txt'));
+
+writeStocks(stream, 'utf-8', () => {
+  stream.end();
+  console.log('fin');
+})
